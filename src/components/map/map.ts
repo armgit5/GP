830bc7ce7e69ee1a14fef550acb2a2953d0601f1
Arm Suwansiri <ysuwansiri@yahoo.com>
2017-12-07 12:25:07 +0700

    added native google maps and enabled traffic

--- src/components/map/map.ts
+++ src/components/map/map.ts
@@ -17,7 +17,7 @@ import {
   CameraPosition,
   MarkerOptions,
   Marker
- } from '@ionic-native/google-maps';
+} from '@ionic-native/google-maps';
 
 /**
  * Generated class for the MapComponent component.
@@ -64,6 +64,7 @@ export class MapComponent implements OnInit, OnDestroy {
     private googleMaps: GoogleMaps) {
 
     this.getALocationLine('rw07invSPBbGv1oY7hcViS83yrR2');
+
   }
 
   private getALocationLine($key: string) {
@@ -74,10 +75,55 @@ export class MapComponent implements OnInit, OnDestroy {
     );
   }
 
+  loadMap() {
+
+        let mapOptions: GoogleMapOptions = {
+          camera: {
+            target: {
+              lat: 43.0741904,
+              lng: -89.3809802
+            },
+            zoom: 18,
+            tilt: 30
+          }
+        };
+
+        let element: HTMLElement = document.getElementById('map');
+
+        this.map = this.googleMaps.create(element, mapOptions);
+
+        // Wait the MAP_READY before using any methods.
+        this.map.one(GoogleMapsEvent.MAP_READY)
+          .then(() => {
+            console.log('Map is ready!');
+
+            // Now you can use all methods safely.
+            this.map.addMarker({
+                title: 'Ionic',
+                icon: 'blue',
+                animation: 'DROP',
+                position: {
+                  lat: 43.0741904,
+                  lng: -89.3809802
+                }
+              })
+              .then(marker => {
+                marker.on(GoogleMapsEvent.MARKER_CLICK)
+                  .subscribe(() => {
+                    alert('clicked');
+                  });
+              });
+
+            this.map.setTrafficEnabled(true);
+
+          });
+      }
+
   ngOnInit() {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      this.loadMap();
       this.onLocate();
       setInterval(() => {
         this.watchPosition();
